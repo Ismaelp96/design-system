257109392e8bc8147b56b979637c175d0970779c
ismaelp96 <ismael6991@live.com>
2025-06-04 11:29:04 -0300

    feat: configurando Monorepo

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
+Construindo Design System, entender como documentar os componentes do React e descobrir diversas ferramentas que vão nos ajudar desde a implementação do Design System até sua publicação, como Storybook, changesets e muito mais.
+
 # Components
 
 - [] Text
---
+++ packages/react/dist/index.js
@@ -0,0 +1,18 @@
+// ../tokens/dist/index.mjs
+var colors = {
+  white: "#FFF",
+  black: "#000",
+  gray100: "#E1E1E6",
+  gray200: "#A9A9B2",
+  gray400: "#7C7C8A",
+  gray600: "#323238",
+  gray800: "#202024",
+  gray900: "#121214",
+  green300: "#00B37E",
+  green500: "#00875F",
+  green700: "#015F43",
+  green900: "#00291D"
+};
+
+// src/index.ts
+console.log(colors);
