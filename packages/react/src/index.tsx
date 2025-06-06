c6b7a33f7c95006732619a856f97ba0a4d128fb2
Ismaelp96 <ismael6991@live.com>
2025-06-06 09:37:20 -0300

    configuração do Stitches #2

--- packages/react/src/index.tsx
+++ packages/react/src/index.tsx
@@ -1,5 +1,10 @@
-import { colors } from '@ignite-ui/tokens';
+import { styled } from './styles';
+
+const Button = styled('button', {
+	fontFamily: '$default',
+	backgroundColor: '$gray900',
+});
 
 export function App() {
-	return <h1 style={{ color: colors.green300 }}>Hello World</h1>;
+	return <Button>Hello World</Button>;
 }
