9ee542537d530e06b878ab592bfdde33129095fa
Ismaelp96 <ismael6991@live.com>
2025-06-10 10:28:09 -0300

    feat: componente Heading #3

---
+++ packages/docs/src/stories/Heading.stories.tsx
@@ -0,0 +1,29 @@
+import type { Meta, StoryObj } from '@storybook/react-vite';
+import { Heading } from '@ignite-ui/react';
+
+const meta = {
+	title: 'Typography/Heading',
+	component: Heading,
+	args: {
+		children: 'Custom title',
+	},
+} satisfies Meta<typeof Heading>;
+
+export default meta;
+type Story = StoryObj<typeof Heading>;
+
+export const Primary: Story = {};
+export const CustomTag: Story = {
+	args: {
+		children: 'h1 heading',
+		as: 'h1',
+	},
+	parameters: {
+		docs: {
+			description: {
+				story:
+					'Por padrão o heading sempre será `h2`, mas podemos alterar isso com a propriedade `as`.',
+			},
+		},
+	},
+};
--- packages/react/src/components/index.ts
+++
@@ -1,3 +0,0 @@
-export { Button } from './Button';
-export { Box } from './Box';
-export { Text } from './Text';
